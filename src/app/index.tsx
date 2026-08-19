ec461f7db710fdeb39286e0599189eb2c1cd1e07
dunaimario1014 <dunaimario1014@gmail.com>
2026-08-19 16:54:47 +0200

    logika hozzáadása

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,48 @@
-import {View, Text, TouchableOpacity} from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App(){
+
+  const [product, setProduct] = useState('');
+  const [list, setList] = useState([]);
+
+  const handlePress = () => {
+
+    if(product.trim() !== ''){
+      setList([...list, product]);
+      setProduct('');
+    }
+
+  }
+
   return(
-    
+    <View style={styles.container}>
+
+      <TextInput
+      style={styles.input}
+      onChangeText={(text) => setProduct(text)}
+      value={product}
+      />
+
+      <TouchableOpacity
+      style={styles.button}
+      onPress={handlePress}
+      >
+
+        <Text style={styles.buttonText}>Hozzáadás</Text>
+
+      </TouchableOpacity>
+
+      {list.map((product, index) => <Text key={index}>{product}</Text>)}
+
+    </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 60
+  },
+});
